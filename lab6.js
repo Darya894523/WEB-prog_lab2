6bbb3b9a48731e1475148aca59ae7c83d2620423
Ветошкина, Васильева <vetoshkina-darya555@mail.ru>
2022-11-22 10:32:19 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -39,3 +39,35 @@ function setEnglish () {
     document.getElementById ('ok-button').value = 'ok';
     document.getElementById ('reset-button').value = 'reset';
 }
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert ('Привет, '+userName + ' ' + userSurname + '\n' + 'Ваш возраст:' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb('+ r + ',' + g + ',' + b + ')';
+
+    document.getElementById('user-data').style.backgroundColor = color;
+
+}
+function showAlertstudent (){
+    alert ('Привет, это студенты ФБИ-02 Васильева, Ветошкина');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb('+ r + ',' + g + ',' + b + ')';
+
+    document.getElementById('student').style.backgroundColor = color;
+
+}
+function showAlertfooter () {
+    alert ('Пока-пока');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb('+ r + ',' + g + ',' + b + ')';
+
+    document.getElementById('footer').style.backgroundColor = color;
+}
