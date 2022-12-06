106ed651c73351dd385762415c94cae5c989f8db
Ветошкина, Васильева <vetoshkina-darya555@mail.ru>
2022-12-06 09:49:02 +0700

    Лесенка

--- lab7.js
+++ lab7.js
@@ -31,3 +31,16 @@ function showAlert() {
     document.getElementById('user-data').style.backgroundColor = color;
 
 }
+function makeStairs() {
+    let stairs = document.querySelector('#stairs');
+    let stepCount = 20;
+    for (let i = 0; i<stepCount; i++){
+        stairs.innerHTML += '<div class="step"></div>';
+    }
+    let steps = document.querySelectorAll('.step');
+    for (let i = 0; i<steps.length; i++){
+        steps [i].style.left = (30 + 10*i) + 'px';
+        steps [i].style.top = (300 + 80*i) + 'px';
+        steps[i].innerHTML = 'Блок номер' + (i+1);
+    }
+}
