3ce78e954abe6ae062c714e9bf8694a911ea65e8
Ветошкина, Васильева <vetoshkina-darya555@mail.ru>
2022-12-20 09:38:05 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,16 @@ function clickMe(button) {
     else
        button.innerHTML = 'Щелчок';
 }
+
+function clickAlisa(button) {
+    if(button.innerHTML == 'Алиса')
+         button.innerHTML = 'Васильева';
+    else
+       button.innerHTML = 'Алиса';
+}
+function clickDasha(button) {
+    if(button.innerHTML == 'Дарья')
+         button.innerHTML = 'Ветошкина';
+    else
+       button.innerHTML = 'Дарья';
+}
