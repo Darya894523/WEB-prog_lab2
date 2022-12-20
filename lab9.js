00e5e4b08dcdba335664fdcbce3f8b51412a3567
Ветошкина, Васильева <vetoshkina-darya555@mail.ru>
2022-12-20 09:50:31 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -17,3 +17,20 @@ function clickDasha(button) {
     else
        button.innerHTML = 'Дарья';
 }
+
+function clickthiss(button) {
+    if(button.innerHTML == 'Нажми меня'){
+       button.innerHTML = 'Ещё раз нажми';
+       button.style.color= 'red';}
+    else
+    if (button.innerHTML == 'Ещё раз нажми'){
+        button.innerHTML = 'Ещё';
+        button.style.color= 'green';}
+    else
+    if (button.innerHTML == 'Ещё'){
+        button.innerHTML = 'Начать сначала';
+        button.style.color= 'yellow';}
+    else
+       {button.innerHTML = 'Нажми меня';
+       button.style.color= 'pink';}
+}
